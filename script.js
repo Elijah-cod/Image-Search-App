cb6b653d992fee59d67a6cd641a6a957d179f48e
Elijah <iamngunjiri@gmail.com>
2023-11-09 19:19:06 +0300

    The app is working as it should, but the images on the home screen are not looking as they should

--- script.js
+++ script.js
@@ -1,35 +1,96 @@
 const apiKey = 'bEMG5NiP29-la9o1En4kcImhoDjpULTb2TfqGQFDBv8'
-const url = "https://api.unsplash.com/search/photos?page=1$&query=dog&client_id=bEMG5NiP29-la9o1En4kcImhoDjpULTb2TfqGQFDBv8"
+
 
 const inputBox = document.getElementById('search-input')
-const searchButton = document.getElementById('search')
 const searchContainer = document.querySelector('.search-results')
+const showMore = document.getElementById('show-more-button')
+// const search = document.getElementById('search')
+
+let page = 1;
+let searchItem = ''
 
 
-async function checkImage (){
+
+
+function startSearch() {
+    if (inputBox.value === ''){
+        alert("No input entered!")
+    } else {
+        searchItem = `${inputBox.value.toLowerCase()}`
+        const api = `https://api.unsplash.com/search/photos?page=${page}$&query=${searchItem}&client_id=bEMG5NiP29-la9o1En4kcImhoDjpULTb2TfqGQFDBv8`
+        checkImage(api)
+    }
+    inputBox.value = ''
+    showMore.style.display = 'block'
+    page += 1
+}
+
+
+
+
+async function checkImage (url){
     searchContainer.innerHTML = ''
+
     const response = await fetch(url)
     var data = await response.json()
 
     //Get the length for each result in one page
     const length = data.results.length
 
-
+    //Displaying the searched images
     for (let i=0; i<=length; i++){
         let image = document.createElement("img");
         image.src = `${data.results[i].urls.full}`
+        image.alt = `${data.results[i].description}`
 
-        let description = document.createElement('a')
-        description.href = `${data.results[i].urls.full}`
-        description.textContent = `${data.results[i].description}`
+        let link = document.createElement('a')
+        link.href = `${data.results[i].urls.full}`
 
         // Add both elements to a class
-        image.classList.add("search-result")
-        description.classList.add("search-result")
+        link.classList.add("search-result")
+        image.classList.add("img")
 
-        searchContainer.appendChild(image)
-        // searchContainer.appendChild(description)
+        link.appendChild(image)
+
+        searchContainer.appendChild(link)
     }
 }
 
-checkImage()
+async function handleClick() {
+  const moreLinks = `https://api.unsplash.com/search/photos?page=${page}$&query=${searchItem}&client_id=bEMG5NiP29-la9o1En4kcImhoDjpULTb2TfqGQFDBv8`
+  const response = await fetch(moreLinks)
+  const moreData = await response.json()
+
+  console.log(moreData)
+  console.log(moreLinks)
+
+  console.log(`Length of new data: ${moreData.results.length}`)
+
+  for(let i=page; i<=moreData.total_pages; i++){
+    page += 1
+    //Get the length for each result in one page
+    const length = moreData.results.length
+
+
+        for (let i=0; i<=length; i++){
+            let image = document.createElement("img");
+            image.src = `${moreData.results[i].urls.full}`
+            image.alt = `${moreData.results[i].description}`
+
+            let link = document.createElement('a')
+            link.href = `${moreData.results[i].urls.full}`
+
+            // Add both elements to a class
+            link.classList.add("search-result")
+            image.classList.add("img")
+
+            link.appendChild(image)
+
+            searchContainer.appendChild(link)
+        }
+    }
+}
+
+showMore.onclick = handleClick
+
+
